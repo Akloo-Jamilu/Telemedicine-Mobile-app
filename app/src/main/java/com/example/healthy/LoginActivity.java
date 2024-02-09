b4fc8a66feaf9ebb70e0605cacff7e1ec926f439
Akloo-jamilu <jhasleg126@yahoo.com>
2024-02-09 12:12:51 +0100

    handling form data

--- app/src/main/java/com/example/healthy/LoginActivity.java
+++ app/src/main/java/com/example/healthy/LoginActivity.java
@@ -3,13 +3,15 @@ package com.example.healthy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
+    EditText userName, password;
     Button button;
     TextView textView;
     @Override
@@ -17,9 +19,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        username = findViewById(R.id.editTextTextLoginUsername);
+        userName = findViewById(R.id.editTextTextLoginUsername);
         password = findViewById(R.id.editTextTextPassword);
         button = findViewById(R.id.buttonLogin);
-        textView = findViewById(R.id.textView);
+        textView = findViewById(R.id.textViewNewUser);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newUserName = userName.getText().toString();
+                String newPasword = password.getText().toString();
+
+                if (newUserName.length()==0 || newPasword.length()==0){
+                    Toast.makeText(getApplicationContext(), "please key in your details", Toast.LENGTH_SHORT).show();
+
+                }
+
+                Toast.makeText(getApplicationContext(), "login Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

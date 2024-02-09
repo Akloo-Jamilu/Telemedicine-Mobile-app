ab4dd8f52fd854dc4121d0524a01cf7004dbedf1
Akloo-jamilu <jhasleg126@yahoo.com>
2024-02-09 16:28:16 +0100

    handling registration data and ui

--- app/src/main/java/com/example/healthy/LoginActivity.java
+++ app/src/main/java/com/example/healthy/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.healthy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -42,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }

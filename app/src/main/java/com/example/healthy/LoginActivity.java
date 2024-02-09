119dc7c015a10446d64336f8f78f25b7f37c95ad
Akloo-jamilu <jhasleg126@yahoo.com>
2024-02-09 11:11:03 +0100

    handling form data

--- app/src/main/java/com/example/healthy/LoginActivity.java
+++ app/src/main/java/com/example/healthy/LoginActivity.java
@@ -3,12 +3,23 @@ package com.example.healthy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        username = findViewById(R.id.editTextTextLoginUsername);
+        password = findViewById(R.id.editTextTextPassword);
+        button = findViewById(R.id.buttonLogin);
+        textView = findViewById(R.id.textView);
     }
 }

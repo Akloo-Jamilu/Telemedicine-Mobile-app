2a8f3f28eaec11f0cbd913867ab4a6fb1a78835f
Akloo-jamilu <jhasleg126@yahoo.com>
2024-02-12 09:10:40 +0100

    handling registration data and ui

--- app/src/main/java/com/example/healthy/RegisterActivity.java
+++ app/src/main/java/com/example/healthy/RegisterActivity.java
@@ -3,9 +3,15 @@ package com.example.healthy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText newUser, newEmail, newPassword, newConfirmPassword;
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

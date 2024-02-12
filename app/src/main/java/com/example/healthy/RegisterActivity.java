3d244f54cfbefff337a925267354070aa493424d
Akloo-jamilu <jhasleg126@yahoo.com>
2024-02-12 12:13:30 +0100

    password check

--- app/src/main/java/com/example/healthy/RegisterActivity.java
+++ app/src/main/java/com/example/healthy/RegisterActivity.java
@@ -54,5 +54,33 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    public static boolean isValid(String passwordHere){
+        int f1=0,f2=0,f3=0;
+        if (passwordHere.length() < 8){
+            return false;
+        }else {
+            for (int p = 0; p < passwordHere.length(); p++){
+                if (Character.isLetter(passwordHere.charAt(p))){
+                    f1=1;
+                }
+            }
+            for (int r = 0; r < passwordHere.length(); r++){
+                if (Character.isDigit(passwordHere.charAt(r))){
+                    f2=1;
+                }
+            }
+            for (int s = 0; s < passwordHere.length(); s++){
+                char c = passwordHere.charAt(s);
+                if (c >= 33 && c <= 46 || c == 64){
+                    f3=1;
+                }
+            }
+            if (f1 == 1 && f2 == 1 && f3 == 1)
+                return true;
+            return false;
+        }
     }
 }

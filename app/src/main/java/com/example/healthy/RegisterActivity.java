25e9bc72f2e647d1ecf64a59e57901cee87e0da7
Akloo-jamilu <jhasleg126@yahoo.com>
2024-02-12 11:46:54 +0100

    handling registration data and ui

--- app/src/main/java/com/example/healthy/RegisterActivity.java
+++ app/src/main/java/com/example/healthy/RegisterActivity.java
@@ -2,10 +2,13 @@ package com.example.healthy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -21,5 +24,35 @@ public class RegisterActivity extends AppCompatActivity {
         newEmail = findViewById(R.id.textEmail);
         newPassword = findViewById(R.id.texPassword);
         newConfirmPassword = findViewById(R.id.editTextRegConfirmPassword);
+        button = findViewById(R.id.buttonRegister);
+        textView = findViewById(R.id.textViewExistingUser);
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = newUser.getText().toString();
+                String email = newEmail.getText().toString();
+                String password = newPassword.getText().toString();
+                String confirmPassword = newConfirmPassword.getText().toString();
+
+                if (user.length() == 0 || email.length() == 0 || password.length() == 0 || confirmPassword.length() == 0){
+                    Toast.makeText(getApplicationContext(), "please fill All details", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (password.compareTo(confirmPassword) == 0){
+
+                    }else {
+                        Toast.makeText(getApplicationContext(), "password and confirm password don't match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }

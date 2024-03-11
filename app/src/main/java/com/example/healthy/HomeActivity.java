74c35ddf2f48078631c37c3227bc01e09d666871
Akloo-jamilu <jhasleg126@yahoo.com>
2024-03-11 12:50:23 +0100

    home page ui

--- app/src/main/java/com/example/healthy/HomeActivity.java
+++ app/src/main/java/com/example/healthy/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.healthy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString( "username", " ").toString();
+        Toast.makeText(getApplicationContext(), "welcome"+username, Toast.LENGTH_SHORT).show();
     }
 }

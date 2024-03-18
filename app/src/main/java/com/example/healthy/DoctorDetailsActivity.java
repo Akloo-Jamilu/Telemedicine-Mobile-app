386dc48eb9851c797d9082dbee63974baa4f7844
Akloo-jamilu <jhasleg126@yahoo.com>
2024-03-18 14:16:47 +0100

    family doctor ui

--- app/src/main/java/com/example/healthy/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/healthy/DoctorDetailsActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
+    TextView tv;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
 
-        TextView tv = findViewById(R.id.textViewDDTitle);
+        tv = findViewById(R.id.textViewDDTitle);
         Intent it = getIntent();
         String title = it.getStringExtra("title");
         tv.setText(title);
+
+
     }
 }

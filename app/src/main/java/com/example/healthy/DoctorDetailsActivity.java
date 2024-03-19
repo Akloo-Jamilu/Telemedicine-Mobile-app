c5c2b783246e9d74488a31f93a9a4751afd7ae8c
Akloo-jamilu <jhasleg126@yahoo.com>
2024-03-19 16:27:53 +0100

    doctor details menu ui

--- app/src/main/java/com/example/healthy/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/healthy/DoctorDetailsActivity.java
@@ -112,10 +112,11 @@ public class DoctorDetailsActivity extends AppCompatActivity {
             item.put("line5", "cons fees:"+doctorDetail [i][4]+"/-");
             list.add(item);
         }
-        sa = new SimpleAdapter(this,R.layout.multi_lines,
+        sa = new SimpleAdapter(this,list,
+                R.layout.multi_lines,
                 new String[]{"line1", "line2", "line3", "line4", "line5"}
-                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d}
-        )
+                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e}
+        );
     }
 }
 
